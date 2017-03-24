d425f29770557ae81c11803d4f65552b752cad87
jmx <mingxin.ji@winsigns.com.cn>
2017-03-24 11:25:00 +0800

    测试对jenkins的触发

--- test.sh
+++ test.sh
@@ -1,7 +1,1 @@
-CURR_TAG=${COMMIT_BUILD_TYPE}${COMMIT_BUILD_NUMBER}
-
-for name in discovery-service fund-service trade-service invest-service inventory-service web-gateway sequence-service
-do
-    echo "docker tag docker-registry:5000/${name}:${CURR_TAG} docker-registry:5000/${name}:latest"
-done
-
+#Do nothing but trigger the jenkins
